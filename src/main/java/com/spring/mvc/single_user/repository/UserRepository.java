74674be9085332cd10bc1155c546711cb0e9da70
foxlo <foxlo@DESKTOP-TRH15VU>
2020-12-29 13:52:50 +0800

    SpringData

---
+++ src/main/java/com/spring/mvc/single_user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.spring.mvc.single_user.repository;
+
+import com.spring.mvc.single_user.entities.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+/*
+只需定義 interface 的規格, 實作內容是由 SpringData 幫你做
+<User, Long> -> User 指的是Entity的類別名, Long 指的是 @Id 的資料型別
+*/
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+    
+}
---
+++ src/test/java/single_user/Test1.java
@@ -0,0 +1,16 @@
+package single_user;
+
+import com.spring.mvc.single_user.entities.User;
+import com.spring.mvc.single_user.repository.UserRepository;
+import java.util.Date;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test1 {
+    @Test
+    public void t1(){
+        ClassPathXmlApplicationContext ctx = 
+                new ClassPathXmlApplicationContext("springMVC-servlet.xml");
+    
+   } 
+}
